bb30184269d7541e4413a46ff41fd24230b4de90
velios00 <98394781+velios00@users.noreply.github.com>
2026-03-01 11:31:46 +0100

    cleaned comments and empty files

--- src/smell_detector/crew.py
+++ src/smell_detector/crew.py
@@ -5,9 +5,6 @@ from typing import List
 from smell_detector.tools.custom_tool import CloneRepoTool
 from smell_detector.tools.custom_tool import ListRepoFilesTool
 from smell_detector.tools.custom_tool import ReadRepoFileTool
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
 class SmellDetector():
@@ -23,13 +20,13 @@ class SmellDetector():
             config=self.agents_config['repo_cloner'],
             tools=[CloneRepoTool()],
             verbose=True,
-            max_iter=3,  # il task è semplice, non serve girare a lungo
+            max_iter=3,
         )
 
     @agent
     def code_smell_analyzer(self) -> Agent:
         return Agent(
-            config=self.agents_config['code_smell_analyzer'], # type: ignore[index]
+            config=self.agents_config['code_smell_analyzer'], 
             tools=[ListRepoFilesTool(), ReadRepoFileTool()],
             verbose=True
         )
@@ -37,14 +34,10 @@ class SmellDetector():
     @agent
     def report_generator(self) -> Agent:
         return Agent(
-            config=self.agents_config['report_generator'], # type: ignore[index]
+            config=self.agents_config['report_generator'], 
             verbose=True
         )
-
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
-
+    
     @task
     def clone_repo_task(self) -> Task:
         return Task(
@@ -54,13 +47,13 @@ class SmellDetector():
     @task
     def smell_analysis_task(self) -> Task:
         return Task(
-            config=self.tasks_config['smell_analysis_task'], # type: ignore[index]
+            config=self.tasks_config['smell_analysis_task'],
         )
     
     @task
     def report_generation_task(self) -> Task:
         return Task(
-            config=self.tasks_config['report_generation_task'], # type: ignore[index]
+            config=self.tasks_config['report_generation_task'],
             output_file='report.md'
         )
     
@@ -69,13 +62,10 @@ class SmellDetector():
     @crew
     def crew(self) -> Crew:
         """Creates the SmellDetector crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
 
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
         )
--- src/smell_detector/test.py
+++
@@ -1,9 +0,0 @@
-from smell_detector.tools.custom_tool import CloneRepoTool
-
-tool = CloneRepoTool()
-result = tool.run(
-    repo_url="https://github.com/nerdschoolbergen/code-smells",
-    dest_dir="cloned_repo"
-)
-
-print(result)
